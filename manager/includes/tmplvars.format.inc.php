edc4e0e5c8d495f176fd04f662f85c07c86b1885
yama <yamamoto@kyms.jp>
2019-01-14 19:44:03 +0900

    Code cleanup

--- manager/includes/tmplvars.format.inc.php
+++ manager/includes/tmplvars.format.inc.php
@@ -1,9 +1,24 @@
 <?php
 function getTVDisplayFormat($name,$value,$format,$paramstring='',$tvtype='',$docid='', $sep='')
-		{global $modx;return $modx->tvProcessor($value,$format,$paramstring,$name,$tvtype,$docid,$sep);}
+{
+	global $modx;
+	return $modx->tvProcessor($value,$format,$paramstring,$name,$tvtype,$docid,$sep);
+}
+
 function decodeParamValue($s)
-		{global $modx;return $modx->decodeParamValue($s);}
+{
+	global $modx;
+	return $modx->decodeParamValue($s);
+}
+
 function parseInput($src, $delim='||', $type='string', $columns=true)
-		{global $modx;return $modx->parseInput($src, $delim, $type, $columns);}
+{
+	global $modx;
+	return $modx->parseInput($src, $delim, $type, $columns);
+}
+
 function getUnixtimeFromDateString($value)
-		{global $modx;return $modx->getUnixtimeFromDateString($value);}
+{
+	global $modx;
+	return $modx->getUnixtimeFromDateString($value);
+}
